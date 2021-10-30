ab4391a262ffa627dd7785a968ac6b303be527be
juliano.basso.whitefox <julianofritz@gmail.com>
2021-10-30 14:35:30 -0300

    connect php to elastic app

--- src/index.php
+++ src/index.php
@@ -1,1 +1,17 @@
 <?php
+
+require '../vendor/autoload.php';
+
+// Create an elastic  client
+$client = Elasticsearch\ClientBuilder::create()
+    ->build();
+
+// Generate an  index
+$params = [
+    'index' => 'my_index',
+    'id'    => 'my_id',
+    'body'  => ['testField' => 'abc']
+];
+
+$response = $client->index($params);
+print_r($response);
